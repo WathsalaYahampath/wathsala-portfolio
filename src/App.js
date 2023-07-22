6758eaa1718104c2e90ee86d0a27215a2db30d2b
WathsalaYahampath <yahampa-se19025@stu.kln.ac.lk>
2023-07-22 20:27:58 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import HomeComponent from "./pages/Home/HomeComponent";
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <HomeComponent/>
+      
+    </div>
+  );
+}
+
+export default App;
---
+++ src/pages/Home/HomeComponent.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Navbar from '../../components/Navbar/Navbar.js'
+
+export default function HomeComponent() {
+  return (
+    <div>
+        
+
+        <Navbar/>
+    </div>
+  )
+}
